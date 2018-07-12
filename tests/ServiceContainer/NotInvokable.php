2bc53fdc38c98bd380016cec581ebe24a343fc78
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-07-12 23:25:53 +0300

    Add "NotInvokable" class for testing

---
+++ tests/ServiceContainer/NotInvokable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tlumx (https://tlumx.com/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/tlumx-servicecontainer
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/tlumx-servicecontainer/blob/master/LICENSE  (MIT License)
+ */
+namespace Tlumx\Tests\ServiceContainer;
+
+class NotInvokable
+{
+
+}
